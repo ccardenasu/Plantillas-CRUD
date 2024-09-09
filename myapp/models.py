81f45e1ca887e24460674848b2bfcc2bd7444b05
PC_empresa_wsl <ccardenasu@gmail.com>
2024-09-09 09:32:32 -0500

    if para tipo de servicio y tipo de configracion para amplion cn vpn y vpls

--- myapp/models.py
+++ myapp/models.py
@@ -4,26 +4,14 @@ import pytz
 
 class Datos(models.Model):
     TIPO_CONFIGURACION_CHOICES = [
-        ('ampliacion_VPN_Jun', 'Ampliacion VPN Jun'),
-        ('Ampliacion_ADI_Alcatel', 'Ampliacion ADI Alcatel'),
-        ('BGP_VPN_Jun', 'BGP VPN Jun'),
-        ('Ampliacion_ADI_Juniper', 'Ampliacion ADI Juniper'),
-        ('Ampliacion_ADI_Alcatel_XCON', 'Ampliacion ADI Alcatel XCON'),
-        ('Ampliacion_ADI_Alcatel_VPL', 'Ampliacion ADI Alcatel VPL'),
-        ('Ampliacion_VPLS_Juniper', 'Ampliacion VPLS Juniper'),
-        ('Instalacion_VPN_Juniper', 'Instalacion VPN Juniper'),
-        ('Instalacion_ADI_Alcatel', 'Instalacion ADI Alcatel'),
-        ('Instalacion_ADI_Juniper', 'Instalacion ADI Juniper'),
-        ('Instalacion_ADI_Alcatel_VPL', 'Instalacion ADI Alcatel VPL'),
-        ('Instalacion_ADI_Alcatel_Xco', 'Instalacion ADI Alcatel Xco'),
-        ('Instalacion_VPLS_Juniper', 'Instalacion VPLS Juniper'),
-        ('Instalacion_ADI_JUN_XCON', 'Instalacion ADI JUN XCON'),
-        ('Instalacion_ADI_irs', 'Instalacion ADI irs'),
-        ('Instalacion_VPLS_J_p2p', 'Instalacion VPLS J p2p'),
-        ('Instalacion_NNI_VRF_GRIS_AZ', 'Instalacion NNI VRF GRIS AZ'),
+        ('', ''),
+        ('Ampliacion', 'Ampliacion'),
+        ('BGP', 'BGP'),
+        ('Alta', 'Alta'),
     ]
 
     TIPO_SERVICIO_CHOICES = [
+        ('', ''),
         ('VPN', 'VPN'),
         ('VPLS', 'VPLS'),
         ('ADI', 'ADI'),
@@ -43,10 +31,10 @@ class Datos(models.Model):
     bundle_ether = models.CharField(max_length=100, choices=BUNDLE_ETHER_CHOICES, default='N/A')
     cfs = models.CharField(max_length=100)
     tipo_configuracion = models.CharField(max_length=100, choices=TIPO_CONFIGURACION_CHOICES, default='default_config')
-    tipo_servicio = models.CharField(max_length=100, choices=TIPO_SERVICIO_CHOICES, default='VPN')
-    rfs_ip_port = models.CharField(max_length=100, default='default_rfs_ip_port')
-    cliente = models.CharField(max_length=100, default='default_cliente')
-    sede = models.CharField(max_length=100, default='default_sede')
+    tipo_servicio = models.CharField(max_length=100, choices=TIPO_SERVICIO_CHOICES, default='')
+    rfs_ip_port = models.CharField(max_length=100, blank=True, null=True)
+    cliente = models.CharField(max_length=100, blank=True, null=True)
+    sede = models.CharField(max_length=100, blank=True, null=True)
     dko = models.CharField(max_length=100, blank=True, null=True)
     sw = models.CharField(max_length=100, blank=True, null=True)
     interface_sw = models.CharField(max_length=100, blank=True, null=True)
@@ -61,7 +49,7 @@ class Datos(models.Model):
     sv = models.CharField(max_length=100, blank=True, null=True)
     cv = models.CharField(max_length=100, blank=True, null=True)
     bw = models.CharField(max_length=100, blank=True, null=True)
-    wan = models.CharField(max_length=100, default='default_sede')
+    wan = models.CharField(max_length=100, default='0.0.0.0/0')
     wanv6 = models.CharField(max_length=100, blank=True, null=True)
     asn = models.CharField(max_length=100, blank=True, null=True)
     lan = models.CharField(max_length=100, blank=True, null=True)
