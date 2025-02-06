84f1b598cb3224aec3d829b9d9ccce5e1d2023bd
carlos <ccardenasu@gmail.com>
2025-02-06 07:25:28 -0500

    06-02-2026

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,15 +3,25 @@ from django.contrib import admin
 from .models import Datos
 
 class DatosAdmin(admin.ModelAdmin):
-    list_display='cfs','dko','IES','tipo_configuracion','tipo_servicio','tipo_equipo','rfs_ip_port','rfs_ip_port_b','rfs_ip_port_nid','cliente','sede','sede_b','sw','interface_sw','sw_b',
-    'interface_sw_b','vrf','rd','unit','unit_nid','unit_b','vt','sv','cv','sv_b','cv_b','bw','bw_plus','bw_Exchange','wan','wanv6','asn','lan','lanv6','lbcpe','lnnid','be','bundle_ether','pe',
-    'pe_vpls_a','pe_vpls_b','interface_pe','interface_pe_vpls_a','interface_pe_vpls_b','lb_pe_vpls_a','lb_pe_vpls_b','puertos_lag','lag','bundle_ether_b','pe_b','interface_pe_b','port_breakout','root_port','child_port',
-    'encap_type','mtu','lag_administrative_key',
+    list_display = [
+    'IES', 'asn', 'be', 'bundle_ether', 'bundle_ether_b', 'bw', 'bw_Exchange', 'bw_plus', 'cfs', 'child_port', 
+    'cliente', 'cv', 'cv_b', 'dko', 'encap_type', 'interface_pe', 'interface_pe_b', 'interface_pe_vpls_a', 
+    'interface_pe_vpls_b', 'interface_sw', 'interface_sw_b', 'lag', 'lag_administrative_key', 'lan', 'lanv6', 
+    'lb_pe_vpls_a', 'lb_pe_vpls_b', 'lbcpe', 'lnnid', 'mtu', 'pe', 'pe_b', 'pe_vpls_a', 'pe_vpls_b', 'port_breakout', 
+    'puertos_lag', 'rd', 'rfs_ip_port', 'rfs_ip_port_b', 'rfs_ip_port_nid', 'root_port', 'sede', 'sede_b', 'sv', 
+    'sv_b', 'sw', 'sw_b', 'tipo_configuracion', 'tipo_equipo', 'tipo_servicio', 'unit', 'unit_b', 'unit_nid', 'vrf', 
+    'vrf_init','vt', 'wan', 'wanv6'
+    ]
     readonly_fields=('created_at','updated_at')
-    search_fields='cfs','dko','IES','tipo_configuracion','tipo_servicio','tipo_equipo','rfs_ip_port','rfs_ip_port_b','rfs_ip_port_nid','cliente','sede','sede_b','sw','interface_sw','sw_b',
-    'interface_sw_b','vrf','rd','unit','unit_nid','unit_b','vt','sv','cv','sv_b','cv_b','bw','bw_plus','bw_Exchange','wan','wanv6','asn','lan','lanv6','lbcpe','lnnid','be','bundle_ether','pe',
-    'pe_vpls_a','pe_vpls_b','interface_pe','interface_pe_vpls_a','interface_pe_vpls_b','lb_pe_vpls_a','lb_pe_vpls_b','puertos_lag','lag','bundle_ether_b','pe_b','interface_pe_b','port_breakout','root_port','child_port',
-    'encap_type','mtu','lag_administrative_key',
+    search_fields = [
+    'IES', 'asn', 'be', 'bundle_ether', 'bundle_ether_b', 'bw', 'bw_Exchange', 'bw_plus', 'cfs', 'child_port', 
+    'cliente', 'cv', 'cv_b', 'dko', 'encap_type', 'interface_pe', 'interface_pe_b', 'interface_pe_vpls_a', 
+    'interface_pe_vpls_b', 'interface_sw', 'interface_sw_b', 'lag', 'lag_administrative_key', 'lan', 'lanv6', 
+    'lb_pe_vpls_a', 'lb_pe_vpls_b', 'lbcpe', 'lnnid', 'mtu', 'pe', 'pe_b', 'pe_vpls_a', 'pe_vpls_b', 'port_breakout', 
+    'puertos_lag', 'rd', 'rfs_ip_port', 'rfs_ip_port_b', 'rfs_ip_port_nid', 'root_port', 'sede', 'sede_b', 'sv', 
+    'sv_b', 'sw', 'sw_b', 'tipo_configuracion', 'tipo_equipo', 'tipo_servicio', 'unit', 'unit_b', 'unit_nid', 'vrf', 
+    'vrf_init','vt', 'wan', 'wanv6'
+    ]
 
 
 admin.site.register(Datos, DatosAdmin)
--- myapp/templates/myapp/IPSA.py
+++ myapp/templates/myapp/IPSA.py
@@ -5,165 +5,92 @@ import json
 import subprocess
 import ipaddress
 
+def leer_archivo(data_path):
+    try:
+        with open(data_path, "r", encoding="utf-8") as file:
+            return file.read()
+    except Exception as e:
+        print(json.dumps({'error': f"Error al leer el archivo: {e}"}))
+        return None
+
+def buscar_primera_coincidencia(patrones, texto):
+    for patron in patrones:
+        resultado = re.search(patron, texto)
+        if resultado:
+            return resultado.group(1).strip()
+    return "No encontrado"
+
+def ejecutar_enlace(cfs):
+    url = f"http://dkvapi.lat.intranet/gpin.html?CFSID={cfs}&ActCountry=MEX&ActUCase=PRO&DVReadOnly=0"
+    subprocess.Popen(['wslview', url])
 
 def main(cfs):
-    # Ruta al archivo de datos usando el valor de CFS
     data_path = os.path.join('data', f'{cfs}.txt')
 
-    # Verificar si el archivo existe
     if not os.path.exists(data_path):
         print(json.dumps({'error': f"El archivo {data_path} no existe."}))
         return
 
-    # Leer el contenido del archivo con codificación 'utf-8'
-    try:
-        with open(data_path, "r", encoding="utf-8") as file:
-            texto = file.read()
-    except Exception as e:
-        print(json.dumps({'error': f"Error al leer el archivo: {e}"}))
+    texto = leer_archivo(data_path)
+    if texto is None:
         return
 
-    def buscar_primera_coincidencia(patron, texto):
-        """
-        Busca la primera coincidencia de un patrón en el texto.
-        """
-        resultado = re.search(patron, texto)
-        return resultado.group(1).strip() if resultado else "No encontrado"
+    patrones_busqueda = {
+        'configuracion': [r'(Alta)'],
+        'pais': [r'(MEXICO)'],
+        'cliente': [r'(?i)Cliente:\s*(.*?)(?=\n\S)', r'(?i)CLIENTE:\s*(.*?)(?=\n\S)'],
+        'sede': [r'(?i)Sede:\s*(.*)'],
+        'dko': [r'DKO:\s*(\d+)'],
+        'cfs': [r'CID-CFS :\s*(\d+)', r'CFS-CID:\s*(\d+)', r'CID-CFS:\s*(\d+)', r'CFS:\s*(\d+)', r'CFS\s*(\d+)'],
+        'rfs_ip_port': [r'RFS IP PORT:\s*(\d+)', r'RFS IP Port:\s*(\d+)', r'IP Port :\s*(\d+)', r'(\d+)\s*IP Port\s*', r'\*:\s*RFS(\d+)\s*\( IP Port \)', r'\*:\s*(\d+)\s*\(IP Port\)'],
+        'rfs_ip_port_nid': [r'NID :\s*(\d+)', r'NID:\s*(\d+)', r'\*:\s*(\d+)\s*\(NID\)'],
+        'vlan': [r'VLAN\s*(\d+)', r'VLAN ASIGNADA:\s*(\d+)'],
+        'sw': [r'DEVICE:\s*([^\s,]+)', r'SW:\s*([^\s,]+)', r'DEVICE:\s*([^\s]+)'],
+        'bw': [r'@(\d+[MG]b)'],
+        'interface_sw': [r'DEVICE:\s*[^\s,]+\s+(Te\d+/\d+/\d+/\d+)'],
+        'vrf': [r'VRF:\s*(.*)'],
+        'asn': [r'AS_BGP:\s*(\d+)'],
+        'wan': [r'IP_WAN:\s*([\d./]+)'],
+        'lbcpe': [r'CPE :\s*(\d+)'],
+        'tipo': [r'(Internet)'],
+    }
 
-    def ejecutar_enlace(cfs):
-        """
-        Construye y ejecuta el enlace con el valor de CFS.
-        """
-        url = f"http://dkvapi.lat.intranet/gpin.html?CFSID={cfs}&ActCountry=MEX&ActUCase=PRO&DVReadOnly=0"
-        subprocess.Popen(['wslview', url])
+    resultados = {clave: buscar_primera_coincidencia(patrones, texto) for clave, patrones in patrones_busqueda.items()}
 
-    # Buscar 'Cliente' y 'Sede'
-    cliente_resultado = buscar_primera_coincidencia(r'(?i)Cliente:\s*(.*?)(?=\n\S)', texto)
-    if cliente_resultado == "No encontrado":
-        cliente_resultado = buscar_primera_coincidencia(r'(?i)CLIENTE:\s*(.*?)(?=\n\S)', texto)
-    
-    sede_resultado = buscar_primera_coincidencia(r'(?i)Sede:\s*(.*)', texto)
+    if resultados['wan'] != "No encontrado":
+        try:
+            ip_network = ipaddress.ip_network(resultados['wan'], strict=False)
+            resultados['wan'] = str(ip_network.network_address) + '/' + str(ip_network.prefixlen)
+        except ValueError as e:
+            resultados['wan'] = f"Error al convertir IP: {e}"
 
-    # Reemplazar "Ñ" por "N" en los resultados
-    cliente_resultado = cliente_resultado.replace("Ñ", "N")
-    if sede_resultado != "No encontrado":
-        sede_resultado = sede_resultado.replace("Ñ", "N")
-
-    # Eliminar la parte común entre Cliente y Sede
-    if cliente_resultado in sede_resultado:
-        sede_resultado = sede_resultado.replace(cliente_resultado, "").strip(" -")
-    else:
-        cliente_palabras = cliente_resultado.split()
-        for palabra in cliente_palabras:
-            if palabra in sede_resultado:
-                sede_resultado = sede_resultado.replace(palabra, "").strip(" -")
-
-    dko_resultado = buscar_primera_coincidencia(r'DKO:\s*(\d+)', texto)
-
-    cfs_resultado = buscar_primera_coincidencia(r'CID-CFS :\s*(\d+)', texto)
-    if cfs_resultado == "No encontrado":
-        cfs_resultado = buscar_primera_coincidencia(r'CFS-CID:\s*(\d+)', texto)
-    if cfs_resultado == "No encontrado":
-        cfs_resultado = buscar_primera_coincidencia(r'CID-CFS:\s*(\d+)', texto)
-    if cfs_resultado == "No encontrado":
-        cfs_resultado = buscar_primera_coincidencia(r'CFS:\s*(\d+)', texto)
-    if cfs_resultado == "No encontrado":
-        cfs_resultado = buscar_primera_coincidencia(r'CFS\s*(\d+)', texto)
-
-    rfs_ip_port_resultado = buscar_primera_coincidencia(r'RFS IP PORT:\s*(\d+)', texto)
-    if rfs_ip_port_resultado == "No encontrado":
-        rfs_ip_port_resultado = buscar_primera_coincidencia(r'RFS IP Port:\s*(\d+)', texto)
-    if rfs_ip_port_resultado == "No encontrado":
-        rfs_ip_port_resultado = buscar_primera_coincidencia(r'IP Port :\s*(\d+)', texto)
-    if rfs_ip_port_resultado == "No encontrado":
-        rfs_ip_port_resultado = buscar_primera_coincidencia(r'(\d+)\s*IP Port\s*', texto)
-    if rfs_ip_port_resultado == "No encontrado":
-        rfs_ip_port_resultado = buscar_primera_coincidencia(r'\*:\s*RFS(\d+)\s*\( IP Port \)', texto)
-    if rfs_ip_port_resultado == "No encontrado":
-        rfs_ip_port_resultado = buscar_primera_coincidencia(r'\*:\s*(\d+)\s*\(IP Port\)', texto)
-
-    rfs_ip_port_nid_resultado = buscar_primera_coincidencia(r'NID :\s*(\d+)', texto)
-    if rfs_ip_port_nid_resultado == "No encontrado":
-        rfs_ip_port_nid_resultado = buscar_primera_coincidencia(r'NID:\s*(\d+)', texto)
-    if rfs_ip_port_nid_resultado == "No encontrado":
-        rfs_ip_port_nid_resultado = buscar_primera_coincidencia(r'\*:\s*(\d+)\s*\(NID\)', texto)
-
-
-# Modificar el patrón de búsqueda para incluir 'VLAN'
-    vlan_resultado = buscar_primera_coincidencia(r'VLAN\s*(\d+)', texto)
-    if vlan_resultado == "No encontrado":
-        vlan_resultado = buscar_primera_coincidencia(r'VLAN ASIGNADA:\s*(\d+)', texto)
-    # Modificar el patrón de búsqueda para incluir 'DEVICE'
-    sw_resultado = buscar_primera_coincidencia(r'DEVICE:\s*([^\s,]+)', texto)
-    if sw_resultado == "No encontrado":
-        sw_resultado = buscar_primera_coincidencia(r'SW:\s*([^\s,]+)', texto)
-    if sw_resultado == "No encontrado":
-        sw_resultado = buscar_primera_coincidencia(r'DEVICE:\s*([^\s]+)', texto)
-
-    # Ajustar el patrón de búsqueda para "bw"
-    bw_resultado = buscar_primera_coincidencia(r'@(\d+)([MG]b)', texto)
-
-    # Buscar 'bw'
-    bw_resultado = buscar_primera_coincidencia(r'@(\d+[MG]b)', texto)
-    if bw_resultado != "No encontrado":
-        match = re.match(r'(\d+)([MG]b)', bw_resultado)
+    if resultados['bw'] != "No encontrado":
+        match = re.match(r'(\d+)([MG]b)', resultados['bw'])
         if match:
             bw_valor, unidad = match.groups()
             bw_valor = int(bw_valor)
             if unidad == "Gb":
-                bw_valor *= 1000  # Convertir gigabits a megabits
-            bw_resultado = f"{bw_valor}"
-    else:
-        bw_resultado = "No encontrado"
-    # Ajustar el patrón de búsqueda para "DEVICE" con el estándar especificado
-    interface_sw_resultado = buscar_primera_coincidencia(r'DEVICE:\s*[^\s,]+\s+(Te\d+/\d+/\d+/\d+)', texto)
+                bw_valor *= 1000
+            resultados['bw'] = f"{bw_valor}"
 
-     # Buscar 'vrf'
-    vrf_resultado = buscar_primera_coincidencia(r'VRF:\s*(.*)', texto)
+    # Asignar "ADI" a tipo_servicio si se encuentra "Internet"
+    tipo_servicio = "ADI" if resultados['tipo'] != "No encontrado" else "No encontrado"
 
-     # Buscar 'asn'
-    asn_resultado = buscar_primera_coincidencia(r'AS_BGP:\s*(\d+)', texto)
+    # Asignar "Juniper" a tipo_equipo si se encuentra "MEXICO" y tipo_servicio es "ADI"
+    tipo_equipo = "JUNIPER" if resultados['pais'] != "No encontrado" and tipo_servicio == "ADI" else "No encontrado"
 
-        # Buscar 'wan'
-    wan_resultado = buscar_primera_coincidencia(r'IP_WAN:\s*([\d./]+)', texto)
-    if wan_resultado != "No encontrado":
-        try:
-            ip_network = ipaddress.ip_network(wan_resultado, strict=False)
-            wan_resultado = str(ip_network.network_address) + '/' + str(ip_network.prefixlen)
-        except ValueError as e:
-            wan_resultado = f"Error al convertir IP: {e}"
+    # Asignar "Alta" a tipo_configuracion si se encuentra "Alta"
+    tipo_configuracion = "Alta" if resultados['configuracion'] != "No encontrado" else "No encontrado"
 
-    # Buscar 'lbcpe'
-    lbcpe_resultado = buscar_primera_coincidencia(r'CPE :\s*(\d+)', texto)
-
-    # Ejecutar el enlace si algún resultado es "No encontrado"
-    if cliente_resultado == "No encontrado" or rfs_ip_port_nid_resultado != "No encontrado" or sede_resultado == "No encontrado" or cfs_resultado == "No encontrado" or rfs_ip_port_resultado == "No encontrado" or rfs_ip_port_nid_resultado == "No encontrado":
+    if any(resultados[clave] == "No encontrado" for clave in ['cliente', 'sede', 'cfs', 'rfs_ip_port', 'rfs_ip_port_nid']):
         ejecutar_enlace(cfs)
 
-    print(json.dumps({
-        
-        'lbcpe': lbcpe_resultado,
-        'wan': wan_resultado,
-        'asn': asn_resultado,
-        'vrf': vrf_resultado,
-        'dko': dko_resultado,
-        'cliente': cliente_resultado,
-        'sede': sede_resultado,
-        'cfs': cfs_resultado,
-        'rfs_ip_port': rfs_ip_port_resultado,
-        'rfs_ip_port_nid': rfs_ip_port_nid_resultado,
-        'vlan': vlan_resultado,
-        'sw': sw_resultado,
-        'interface_sw': interface_sw_resultado,
-        'bw': bw_resultado,
-        'lnnid':rfs_ip_port_nid_resultado,
-
-        
+    resultados['tipo_servicio'] = tipo_servicio
+    resultados['tipo_equipo'] = tipo_equipo
+    resultados['tipo_configuracion'] = tipo_configuracion
 
 
-
-
-    }))
+    print(json.dumps(resultados))
 
 if __name__ == "__main__":
     cfs = sys.argv[1] if len(sys.argv) > 1 else ""
